c0c6d43f4ffb96b915b92b9c26daac4c22009384
Maëlle Salmon <maelle.salmon@yahoo.se>
2023-08-21 15:01:31 +0200

    fixes

--- convert.R
+++ convert.R
@@ -1,19 +1,22 @@
 posts <- fs::dir_ls("../cynkraweb/www/_posts")
 
 register_post <- function(path) {
-  cli::cli_text("Copying files for {.path {folder_name}}")
   folder_name <- path |>
     fs::path_file() |>
     fs::path_ext_remove()
-  fs::dir_create(file.path("posts", folder_name))
-  fs::dir_copy(path, file.path("posts", folder_name, "index.qmd"))
+  cli::cli_text("Copying files for {.path {folder_name}}")
+
+  parent_folder <- file.path("blog", "posts", folder_name)
+
+  fs::dir_create(parent_folder)
+  fs::dir_copy(path, file.path(parent_folder, "index.qmd"))
 
   images_folder <- file.path("../cynkraweb/www/blog", folder_name)
   if (fs::dir_exists(images_folder)) {
     cli::cli_text("Copying images for {.path {folder_name}}")
     fs::file_copy(
       fs::dir_ls(images_folder),
-      file.path("posts", folder_name)
+      parent_folder
     )
   }
 }
